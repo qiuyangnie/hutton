b3ee3ac063b132d19fe4977ba5f311a11fed4ed6
Qiuyang Nie <qyang.nie@gmail.com>
2020-09-02 12:47:57 +0100

    Add reverse

--- 06_recursive_functions/test1.hs
+++ 06_recursive_functions/test1.hs
@@ -7,3 +7,11 @@
 product :: Num a => [a] -> a
 product []     = 1
 product (n:ns) = n * Main.product ns
+
+length :: [a] -> Int
+length []     = 0
+length (_:xs) = 1 + Main.length xs
+
+reverse :: [a] -> [a]
+reverse []     = []
+reverse (x:xs) = Main.reverse xs ++ [x]
