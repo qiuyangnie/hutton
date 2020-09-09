fc7f1944548afc65d29e9493a1f950104bc92929
Qiuyang Nie <qyang.nie@gmail.com>
2020-09-09 17:34:43 +0100

    Add max

--- exercise/three/example.hs
+++ exercise/three/example.hs
@@ -4,5 +4,12 @@ say n | mod n 3 == 0 && mod n 5 == 0 = "FizzBuzz"
       | mod n 5 == 0                 = "Buzz"
       | otherwise                    = show n
 
+myFunction :: String -> String
+myFunction = undefined
+
 main :: IO()
 main = mapM_ putStrLn $ map say [1..100]
+
+max :: Integer -> Integer -> Integer
+max n n' | n > n'    = n
+         | otherwise = n'
