0afa8476ca97a26d1c86216bae7c6353f1f214cd
Qiuyang Nie <qyang.nie@gmail.com>
2020-09-11 20:37:48 +0100

    Add filter functions

--- 07_higher-order_functions/test0.hs
+++ 07_higher-order_functions/test0.hs
@@ -7,3 +7,12 @@ map' f xs = [f x | x <- xs]
 map'' :: (a -> b) -> [a] -> [b]
 map'' _ []     = []
 map'' f (x:xs) = f x : map'' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p xs = [x | x <- xs, p x]
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' _ [] = []
+filter'' p (x:xs)
+   | p x         = x : filter'' p xs
+   | otherwise   = filter'' p xs
