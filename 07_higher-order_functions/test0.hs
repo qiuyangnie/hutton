eab390f2f52a1347db678126b7b678e2c48d64ba
Qiuyang Nie <qyang.nie@gmail.com>
2020-09-11 20:16:12 +0100

    Add map functions

--- 07_higher-order_functions/test0.hs
+++ 07_higher-order_functions/test0.hs
@@ -1,3 +1,9 @@
 twice :: (a -> a) -> a -> a
 twice f x = f (f x)
 
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = [f x | x <- xs]
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' _ []     = []
+map'' f (x:xs) = f x : map'' f xs
